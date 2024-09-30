d97f8591ba91e64f16e7538826cf1cd4155c2edb
Prateek Baheti <87934232+prateek-baheti@users.noreply.github.com>
2024-09-30 21:51:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Score from Subarray Minimums/maximum-score-from-subarray-minimums.java
+++ Difficulty: Medium/Maximum Score from Subarray Minimums/maximum-score-from-subarray-minimums.java
@@ -34,14 +34,15 @@ public class Main {
 class Solution {
     // Function to find pair with maximum sum
     public int pairWithMaxSum(List<Integer> arr) {
-        int max=0;
+        // Your code goes here
+        int max=Integer.MIN_VALUE;
         int sum=0;
         for(int i=0;i<arr.size()-1;i++)
         {
             sum=arr.get(i)+arr.get(i+1);
-            max=Math.max(max,sum);
+            max=Math.max(sum,max);
+            sum=0;
         }
         return max;
-        // Your code goes here
     }
 }

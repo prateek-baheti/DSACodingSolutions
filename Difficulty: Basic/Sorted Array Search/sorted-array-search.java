72bbc6f5633b54c701589bee7ba5eeb7a5420458
Prateek Baheti <87934232+prateek-baheti@users.noreply.github.com>
2025-03-17 12:28:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sorted Array Search/sorted-array-search.java
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.java
@@ -30,14 +30,13 @@ class gfg {
 
 class Solution {
     static boolean searchInSorted(int arr[], int k) {
-        // Your code here
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]==k)
-            {
-                return true;
-            }
-        }
-        return false;
+     for(int i=0;i<arr.length;i++)
+     {
+         if(arr[i]==k)
+         {
+             return true;
+         }
+     }
+     return false;
     }
 }

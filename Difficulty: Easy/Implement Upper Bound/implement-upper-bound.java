85efa596d2c304fe61064a5e7eb452d1900c6760
Prateek Baheti <87934232+prateek-baheti@users.noreply.github.com>
2025-04-17 17:08:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Upper Bound/implement-upper-bound.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution sln = new Solution();
+            int ans = sln.upperBound(a, k);
+
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    int upperBound(int[] arr, int target) {
+        // code here
+        int left=0;
+        int right=arr.length-1;
+        int ans=arr.length;;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(arr[mid]>target)
+            {
+                ans=mid;
+                right=mid-1;
+                
+            }else{
+                left=mid+1;
+            }
+        }
+        return ans;
+    }
+}
